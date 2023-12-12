5b4fe5f40cd387038aa1dde5440163d9a65514bf
soraps <srapsode@gmail.com>
2023-12-12 16:07:44 +0100

    appli

--- app.py
+++ app.py
@@ -1,23 +1,68 @@
-# %%
-# ! pip install streamlit
-
-
-# %%
 import streamlit as st
 import json
+
+# Configurer la page pour utiliser le mode large
+st.set_page_config(layout="wide")
+
+# Style personnalisé pour le titre principal et les titres des recettes
+st.markdown("""
+<style>
+.big-font {
+    font-size:60px !important;
+    font-weight: bold;
+    color: #c46e28;
+    text-align: center;
+}
+.recipe-title {
+    font-size:26px;
+    font-weight: bold;
+    color: #cca995;
+    text-align: center;
+}
+.img-container img {
+    width: 100%;  /* Prend toute la largeur de la colonne */
+    height: 300px; /* Hauteur fixe pour toutes les images */
+    object-fit: cover; /* Couvre la zone de l'image tout en conservant ses proportions */
+}
+</style>
+""", unsafe_allow_html=True)
+
+# Titre de la page avec le style personnalisé
+st.markdown('<p class="big-font">Baby Recipe... YUMMY!</p>', unsafe_allow_html=True)
 
 # Charger les données
 with open('recettes.json', 'r', encoding='UTF-8') as f:
     recettes = json.load(f)
 
-# Afficher les recettes
+
+
+# Extraire les tags uniques
+tags_uniques = set()
 for recette in recettes:
-    st.image(recette['url_image'], width=150)
-    st.write(recette['titre'])
+    for tag in recette.get('tags', []):
+        tags_uniques.add(tag)
 
-    st.markdown(f"[Voir la recette]({recette['lien']})")
+# Créer des cases à cocher pour les tags dans la barre latérale
+selected_tags = []
+for tag in tags_uniques:
+    if st.sidebar.checkbox(tag, key=tag):
+        selected_tags.append(tag)
 
-# %%
+# Filtrer les recettes basées sur les tags sélectionnés
+recettes_filtrees = [recette for recette in recettes if any(tag in recette.get('tags', []) for tag in selected_tags)]
 
 
+# Créer des lignes avec 4 recettes chacune
+for i in range(0, len(recettes), 4):
+    cols = st.columns(4)  # Crée 4 colonnes
+    for j in range(4):
+        if i + j < len(recettes):
+            with cols[j]:
+                recette = recettes[i + j]
+                # Utilisation de HTML pour l'image
+                st.markdown(f"<div class='img-container'><img src='{recette['url_image']}' alt='{recette['titre']}'></div>", unsafe_allow_html=True)
+                
+                # Titre de la recette avec style personnalisé
+                st.markdown(f"<p class='recipe-title'>{recette['titre']}</p>", unsafe_allow_html=True)
 
+                st.markdown(f"[Voir la recette]({recette['lien']})", unsafe_allow_html=True)
